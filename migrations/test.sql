2465c6164e728fa9ca28165b0852dad6ebe86c8c
johann2357 <johann2357@gmail.com>
2013-12-20 14:46:36 -0500

    base de datos para prueba

---
+++ migrations/test.sql
@@ -0,0 +1,83 @@
+-- MySQL dump 10.13  Distrib 5.5.34, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: test
+-- ------------------------------------------------------
+-- Server version	5.5.34-0ubuntu0.13.10.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `materials`
+--
+
+DROP TABLE IF EXISTS `materials`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `materials` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(155) NOT NULL,
+  `description` text,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `materials`
+--
+
+LOCK TABLES `materials` WRITE;
+/*!40000 ALTER TABLE `materials` DISABLE KEYS */;
+INSERT INTO `materials` VALUES (1,'Lorem','Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...','2013-12-17 19:34:55','2013-12-17 21:29:21');
+/*!40000 ALTER TABLE `materials` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(155) NOT NULL,
+  `description` text,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  `price` decimal(7,2) NOT NULL,
+  `material_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'Mauris tempus cortina','Honcus semper. Duis faucibus fringilla lectus, nec consectetur diam ullamcorper ac. Suspendisse potenti. Maecenas purus augue, accumsan sed orci eget, hendrerit varius augue. Donec ac purus fringilla, tincidunt nunc at, varius nibh. ','2013-12-17 19:46:45','2013-12-19 02:55:58',66.00,1),(2,'Cortina','Nuevo tipo de cortina','2013-12-17 23:34:33','2013-12-19 02:53:19',99.90,1),(3,'tempus','prueba tempus','2013-12-18 05:52:04','2013-12-18 05:52:04',345.00,1),(4,'Cortina 2','Nueva cortina 2','2013-12-18 23:52:49','2013-12-18 23:52:49',134.00,1),(5,'Cortina 3','Nueva cortina 3','2013-12-18 23:53:40','2013-12-18 23:53:40',130.00,1),(6,'Cortina 1','Nueva cortina 1','2013-12-18 23:54:15','2013-12-18 23:54:15',131.00,1),(7,'Cortina 5','Nueva cortina 5','2013-12-18 23:54:42','2013-12-18 23:54:42',121.00,1),(8,'Cortina 0','Nueva cortina 0','2013-12-18 23:55:27','2013-12-18 23:55:27',101.00,1),(9,'Cortina 4','Nueva cortina 4','2013-12-18 23:56:50','2013-12-18 23:56:50',141.00,1),(10,'Cortina moderna','Nueva cortina moderna','2013-12-18 23:57:20','2013-12-18 23:57:20',99.90,1),(11,'Cortina especial','Nueva cortina especial','2013-12-18 23:58:31','2013-12-18 23:58:31',99.99,1),(12,'Cortina simple','Nueva cortina simple','2013-12-19 02:49:58','2013-12-20 19:30:51',177.00,1),(13,'Cortina 222','Nueva cortina 222','2013-12-19 02:51:39','2013-12-19 02:51:39',666.00,1);
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-12-20 14:44:06
