eda0be75573375a29ee1f9681b3db6f69a4b6dd6
johann2357 <johann2357@gmail.com>
2013-12-23 17:49:22 -0500

    banners

---
+++ cakephp/app/Model/Banner.php
@@ -0,0 +1,68 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Banner Model
+ *
+ */
+class Banner extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+  public $displayField = 'name';
+
+/**
+ * Plugin cakephp-upload
+ * 
+ * @var array
+ */ 
+  public $actsAs = array(
+    'Upload.Upload' => array(
+      'photo' => array(
+        'fields' => array(
+            'dir' => 'photo_dir'
+        )
+      )
+    )
+  );
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+  public $validate = array(
+    'id' => array(
+      'notEmpty' => array(
+        'rule' => array('notEmpty'),
+        //'message' => 'Your custom message here',
+        //'allowEmpty' => false,
+        //'required' => false,
+        //'last' => false, // Stop validation after this rule
+        //'on' => 'create', // Limit validation to 'create' or 'update' operations
+      ),
+    ),
+    'name' => array(
+      'notEmpty' => array(
+        'rule' => array('notEmpty'),
+        //'message' => 'Your custom message here',
+        //'allowEmpty' => false,
+        //'required' => false,
+        //'last' => false, // Stop validation after this rule
+        //'on' => 'create', // Limit validation to 'create' or 'update' operations
+      ),
+    ),
+    'measure' => array(
+      'notEmpty' => array(
+        'rule' => array('notEmpty'),
+        //'message' => 'Your custom message here',
+        //'allowEmpty' => false,
+        //'required' => false,
+        //'last' => false, // Stop validation after this rule
+        //'on' => 'create', // Limit validation to 'create' or 'update' operations
+      ),
+    ),
+  );
+}
---
+++ cakephp/app/View/Banners/admin_edit.ctp
@@ -0,0 +1,23 @@
+<div class="banners form">
+<?php echo $this->Form->create('Banner'); ?>
+	<fieldset>
+		<legend><?php echo __('Admin Edit Banner'); ?></legend>
+	<?php
+		echo $this->Form->input('id');
+		echo $this->Form->input('name');
+		echo $this->Form->input('description');
+		echo $this->Form->input('measure');
+		echo $this->Form->input('photo');
+		echo $this->Form->input('photo_dir');
+	?>
+	</fieldset>
+<?php echo $this->Form->end(__('Submit')); ?>
+</div>
+<div class="actions">
+	<h3><?php echo __('Actions'); ?></h3>
+	<ul>
+
+		<li><?php echo $this->Form->postLink(__('Delete'), array('action' => 'delete', $this->Form->value('Banner.id')), null, __('Are you sure you want to delete # %s?', $this->Form->value('Banner.id'))); ?></li>
+		<li><?php echo $this->Html->link(__('List Banners'), array('action' => 'index')); ?></li>
+	</ul>
+</div>
