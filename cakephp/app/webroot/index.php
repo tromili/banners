aaf5ee550cfb18b098b0f932ece7862d50df0076
tromili <anthonny.gonzales@gmail.com>
2013-12-23 15:24:40 -0500

    vista_index

--- cakephp/app/webroot/index.php
+++ cakephp/app/webroot/index.php
@@ -1,108 +1,198 @@
-<?php
-/**
- * Index
- *
- * The Front Controller for handling every request
- *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @package       app.webroot
- * @since         CakePHP(tm) v 0.2.9
- * @license       http://www.opensource.org/licenses/mit-license.php MIT License
- */
 
-/**
- * Use the DS to separate the directories in other defines
- */
-if (!defined('DS')) {
-	define('DS', DIRECTORY_SEPARATOR);
-}
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Grupo E&f</title>
+    <link rel="Shortcut Icon" type="image/x-icon" href="img/logo.jpg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-/**
- * These defines should only be edited if you have cake installed in
- * a directory layout other than the way it is distributed.
- * When using custom settings be sure to use the DS and do not add a trailing DS.
- */
+    <!-- Le styles -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 20px;
+        padding-bottom: 40px;
+      }
 
-/**
- * The full path to the directory which holds "app", WITHOUT a trailing DS.
- *
- */
-if (!defined('ROOT')) {
-	define('ROOT', dirname(dirname(dirname(__FILE__))));
-}
+      /* Custom container */
+      .container-narrow {
+        margin: 0 auto;
+        max-width: 700px;
+      }
+      .container-narrow > hr {
+        margin: 30px 0;
+      }
 
-/**
- * The actual directory name for the "app".
- *
- */
-if (!defined('APP_DIR')) {
-	define('APP_DIR', basename(dirname(dirname(__FILE__))));
-}
+      /* Main marketing message and sign up button */
+      .jumbotron {
+        margin: 60px 0;
+        text-align: center;
+      }
+      .jumbotron h1 {
+        font-size: 72px;
+        line-height: 1;
+      }
+      .jumbotron .btn {
+        font-size: 21px;
+        padding: 14px 24px;
+      }
 
-/**
- * The absolute path to the "cake" directory, WITHOUT a trailing DS.
- *
- * Un-comment this line to specify a fixed path to CakePHP.
- * This should point at the directory containing `Cake`.
- *
- * For ease of development CakePHP uses PHP's include_path. If you
- * cannot modify your include_path set this value.
- *
- * Leaving this constant undefined will result in it being defined in Cake/bootstrap.php
- *
- * The following line differs from its sibling
- * /lib/Cake/Console/Templates/skel/webroot/index.php
- */
-//define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'lib');
+      /* Supporting marketing content */
+      .marketing {
+        margin: 60px 0;
+      }
+      .marketing p + h4 {
+        margin-top: 28px;
+      }
+      
+    </style>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+    <script type="text/javascript" src="js/s3Slider.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#slider1').s3Slider({
+                timeOut: 4000 
+            });
+        });
+    </script>
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
 
-/**
- * Editing below this line should NOT be necessary.
- * Change at your own risk.
- *
- */
-if (!defined('WEBROOT_DIR')) {
-	define('WEBROOT_DIR', basename(dirname(__FILE__)));
-}
-if (!defined('WWW_ROOT')) {
-	define('WWW_ROOT', dirname(__FILE__) . DS);
-}
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
 
-// for built-in server
-if (php_sapi_name() === 'cli-server') {
-	if ($_SERVER['REQUEST_URI'] !== '/' && file_exists(WWW_ROOT . $_SERVER['PHP_SELF'])) {
-		return false;
-	}
-	$_SERVER['PHP_SELF'] = '/' . basename(__FILE__);
-}
+    <!-- Fav and touch icons -->
+  </head>
 
-if (!defined('CAKE_CORE_INCLUDE_PATH')) {
-	if (function_exists('ini_set')) {
-		ini_set('include_path', ROOT . DS . 'lib' . PATH_SEPARATOR . ini_get('include_path'));
-	}
-	if (!include 'Cake' . DS . 'bootstrap.php') {
-		$failed = true;
-	}
-} else {
-	if (!include CAKE_CORE_INCLUDE_PATH . DS . 'Cake' . DS . 'bootstrap.php') {
-		$failed = true;
-	}
-}
-if (!empty($failed)) {
-	trigger_error("CakePHP core could not be found. Check the value of CAKE_CORE_INCLUDE_PATH in APP/webroot/index.php. It should point to the directory containing your " . DS . "cake core directory and your " . DS . "vendors root directory.", E_USER_ERROR);
-}
+  <body>
 
-App::uses('Dispatcher', 'Routing');
+    <div class="container-narrow">
 
-$Dispatcher = new Dispatcher();
-$Dispatcher->dispatch(
-	new CakeRequest(),
-	new CakeResponse()
-);
+      <div class="masthead">
+        <ul class="nav nav-pills pull-right">
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#">¿Quienes Somos?</a></li>
+          <li><a href="#">Contáctanos</a></li>
+        </ul>
+        <div class="logo"></div>
+      </div>
+      <div id="slider1">
+            <ul id="slider1Content">
+                <li class="slider1Image">
+                    <a href=""><img src="img/banner_1.jpg" alt="banner" /></a>
+                    <span class="bottom"><strong>Title text 1</strong><br />Content text...</span></li>
+                <li class="slider1Image">
+                    <a href=""><img src="img/banner_2.jpg" alt="banner" /></a>
+                    <span class="bottom"><strong>Title text 2</strong><br />Content text...Content text...Content text...Content text...Content text...Content text...Content text...Content text...Content text...Content text...Content text...</span></li>
+                <li class="slider1Image">
+                     <a href=""><img src="img/banner_3.jpg" alt="banner" /></a>
+                    <span class="bottom"><strong>Title text 2</strong><br />Content text...</span></li>
+                <li class="slider1Image">
+                     <a href=""><img src="img/banner_4.jpg" alt="banner" /></a>
+                    <span class="bottom"><strong>Title text 2</strong><br />Content text...</span></li>
+                <div class="clear slider1Image"></div>
+            </ul>
+        </div>
+      <hr>
+
+      <div class="main">
+        <h2>Bienvenidos</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et erat neque. Vivamus fringilla nec nisi eget dignissim. Phasellus vel eros ac urna tincidunt bibendum. Curabitur in nulla elit. Sed sollicitudin porttitor erat, et malesuada nunc. Nunc malesuada metus felis, id fringilla metus ullamcorper id. Sed pharetra quis dui ut consequat. Integer a dictum felis. Suspendisse viverra nisi nec posuere tincidunt. Quisque pharetra convallis viverra. Nulla commodo augue quis purus euismod, at fermentum ante ornare. Nam sed est non quam dapibus hendrerit. Donec id magna vitae tortor elementum vehicula sit amet non odio. Proin nulla dui, imperdiet at massa id, sollicitudin feugiat leo. Aliquam non metus euismod, venenatis ipsum quis, facilisis velit. </p>
+      </div>
+      <hr>
+      <div class="search">
+        <h2>Busqueda</h2>
+      </div>
+      <hr>
+         <h2>Ultimas notiicas</h2>
+      <div class="row-fluid marketing">
+
+        <div class="span6">
+          <h4>Subheading</h4>
+          <h5>12/03/13</h5>
+          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+          <h4>Subheading</h4>
+          <h5>12/03/14</h5>
+          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+          <h4>Subheading</h4>
+          <h5>12/03/15</h5>
+          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        </div>
+
+        <div class="span6">
+          <h4>Subheading</h4>
+          <h5>12/03/13</h5>
+          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+          <h4>Subheading</h4>
+          <h5>12/03/13</h5>
+          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+          <h4>Subheading</h4>
+          <h5>12/03/13</h5>
+          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        </div>
+      </div>
+      <hr>
+      <table class="tablacontactanos">
+        <tr>
+          <td>
+            <h2>Contáctanos</h2>
+            <span>
+              Av. Quiñones 122<br> Alto Selva Algue
+            </span> 
+            <br>
+            
+            <span>
+              (054) 201847  (511)991566865<br> (054) 12323
+            </span> 
+            
+             <br>
+            <span>
+              grupoef@gmail.com
+            </span> 
+           
+             <br>
+            <span>
+              Lunes a Sábado <br> 9:00am. a 8:30pm.
+            </span> 
+          </td>
+          <td>
+            <h2>Escríbenos</h2>
+          </td>
+        </tr>
+      </table>
+      <hr>
+
+      <div class="footer">
+        <p>&copy; 2013 Grupo E&F.</p>
+      </div>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap-transition.js"></script>
+    <script src="js/bootstrap-alert.js"></script>
+    <script src="js/bootstrap-modal.js"></script>
+    <script src="js/bootstrap-dropdown.js"></script>
+    <script src="js/bootstrap-scrollspy.js"></script>
+    <script src="js/bootstrap-tab.js"></script>
+    <script src="js/bootstrap-tooltip.js"></script>
+    <script src="js/bootstrap-popover.js"></script>
+    <script src="js/bootstrap-button.js"></script>
+    <script src="js/bootstrap-collapse.js"></script>
+    <script src="js/bootstrap-carousel.js"></script>
+    <script src="js/bootstrap-typeahead.js"></script>
+
+  </body>
+</html>
